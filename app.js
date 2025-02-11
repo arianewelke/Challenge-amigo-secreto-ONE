fa129299be91d1df68afc246d022dab0a99ce932
Ariane Wwelke <arianewelkep97@gmail.com>
2025-02-11 12:48:55 -0300

    add funcao adicionar nomes a lista

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 let listaDeAmigosSecretos = []; // array para armazenar nomes
+
+//Função para adicionar nomes a lista
+function adicionarAmigo() {
+    let inputNome = document.getElementById("amigo");
+    let nome = inputNome.value.trim(); // Remove espaços extras
+
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+
+    if (listaDeAmigosSecretos.includes(nome)) {
+        alert("Esse nome já foi adicionado!");
+        return;
+    }
+
+    listaDeAmigosSecretos.push(nome);
+    inputNome.value = "";
+    console.log(listaDeAmigosSecretos);
+    let li = document.createElement("li");
+        li.textContent = nome;
+        listaAmigos.appendChild(li);
+}
